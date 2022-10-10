1ad6fd167f2963e2ff81a8d8401beabf1f0cf61b
amanchaudhary01 <aman.patel7474@gmail.com>
2022-10-10 23:19:34 +0530

    Made middleware for the admin .

--- resources/views/layouts/include/admin/navbar.blade.php
+++ resources/views/layouts/include/admin/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="navbar-brand-wrapper d-flex justify-content-center">
       <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">  
-        <a class="navbar-brand brand-logo" href="index.html"><img src="images/logo.svg" alt="logo"/></a>
+        <a class="navbar-brand brand-logo" href="index.html">Ecommerce</a>
         <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg" alt="logo"/></a>
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
           <span class="mdi mdi-sort-variant"></span>
@@ -125,10 +125,17 @@
               <i class="mdi mdi-settings text-primary"></i>
               Settings
             </a>
-            <a class="dropdown-item">
-              <i class="mdi mdi-logout text-primary"></i>
-              Logout
-            </a>
+          
+            <a class="dropdown-item" href="{{ route('logout') }}"
+          
+            onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+             <i class="mdi mdi-logout text-primary"></i>  {{ __('Logout') }}
+         </a>
+
+         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+             @csrf
+         </form>
           </div>
         </li>
       </ul>
